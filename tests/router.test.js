64452dca56f7558b39099fff9e3c94b50ddf18f5
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-11 11:21:53 +0530

    test: add user registration
    add user registration API's test case

--- tests/router.test.js
+++ tests/router.test.js
@@ -2,10 +2,22 @@ import app from '../index';
 import request from 'supertest';
 
 describe('GET /user', function () {
-  it('responds with json', async function () {
+  it('users data with total users count', async function () {
     const response = await request(app).get('/user');
     expect(response.status).toEqual(200);
     expect(typeof response.body.data).toBe('object');
     expect(typeof response.body.total).toBe('number');
+  });
+
+  it('user save & sent email for verify mail address', async function () {
+    const response = await request(app)
+      .post('/user')
+      .send({
+        name: 'john',
+        email: 'username@provider.domain',
+        password: '#1Password',
+      })
+      .set('Accept', 'application/json');
+    expect(response.status).toEqual(200);
   });
 });
