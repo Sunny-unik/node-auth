85341e34155eff1944459baadf4cde3f03f9a8e6
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-11 11:09:32 +0530

    fix: update jest config
    remove watchPathIgnorePatterns option from jest.config because it is not good if we already using modulePaths option

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 /** @type {import('jest').Config} */
 
-const testsDir = '<rootDir>/tests/';
 const config = {
-  modulePaths: [testsDir],
-  watchPathIgnorePatterns: [testsDir],
+  modulePaths: ['<rootDir>/tests/'],
 };
 
 export default config;
