71c5ced33c5ca797622078c772dcc3743644e126
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-11 19:33:08 +0530

    chore: update DB configs

--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 export default async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI +
+        (process.env.NODE_ENV === 'test' ? 'testNodeAuth' : 'NodeAuth') +
+        '?readPreference=primary&retryWrites=true',
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    );
     const { host, port, name } = conn.connection;
 
     console.log(`MongoDB Connected at mongodb://${host}/${port}, DB: ${name}`);
