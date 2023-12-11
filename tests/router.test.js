35939cca45dadbd3c5db8eb76e52fa32adc3f06d
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-11 12:09:42 +0530

    test: add invalid OTP case

--- tests/router.test.js
+++ tests/router.test.js
@@ -2,6 +2,8 @@ import app from '../index';
 import request from 'supertest';
 
 describe('GET /user', function () {
+  let userId;
+
   it('users data with total users count', async function () {
     const response = await request(app).get('/user');
     expect(response.status).toEqual(200);
@@ -18,6 +20,18 @@ describe('GET /user', function () {
         password: '#1Password',
       })
       .set('Accept', 'application/json');
+    userId = response.body.id;
     expect(response.status).toEqual(200);
+  });
+
+  it('user update as verified failed because of invalid OTP', async function () {
+    const response = await request(app)
+      .post('/user/verify')
+      .send({
+        id: userId,
+        otp: 565656,
+      })
+      .set('Accept', 'application/json');
+    expect(response.status).toEqual(500);
   });
 });
