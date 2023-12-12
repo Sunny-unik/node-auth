98c39477555de422a74796c4d6274a7949dae488
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-12 19:50:10 +0530

    test: update tests
    add test to get specified user's data & verify user API using valid OTP

--- src/tests/router.test.js
+++ src/tests/router.test.js
@@ -2,7 +2,7 @@ import app, { appInstance } from '../index';
 import request from 'supertest';
 
 describe('GET /user', function () {
-  let userId;
+  let userId, userOtp;
   afterAll(() => appInstance.close());
 
   it('users data with total users count', async function () {
@@ -29,15 +29,30 @@ describe('GET /user', function () {
     );
   });
 
+  it('user data with otp', async function () {
+    const response = await request(app).get(
+      `/user/${userId}/?query=_id%20name%20otp%20`
+    );
+    expect(response.status).toEqual(200);
+    userOtp = response.body.data?.otp;
+    expect(typeof userOtp).toBe('number');
+  });
+
   it('user update as verified failed because of invalid OTP', async function () {
     const response = await request(app)
       .post('/user/verify')
-      .send({
-        id: userId,
-        otp: 565656,
-      })
+      .send({ id: userId, otp: 565656 })
       .set('Accept', 'application/json');
     expect(response.status).toEqual(500);
     expect(response.body.message).toEqual('Invalid OTP');
+  });
+
+  it('user update as verified successfully', async function () {
+    const response = await request(app)
+      .post('/user/verify')
+      .send({ id: userId, otp: userOtp })
+      .set('Accept', 'application/json');
+    expect(response.status).toEqual(200);
+    expect(typeof response.body.data).toEqual('object');
   });
 });
