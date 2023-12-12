c8e27c3fa46833e79eaa21cb424fb47ccb184633
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-12 18:23:30 +0530

    refactor: add timestamps in users model

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,15 +1,18 @@
 import mongoose, { Schema } from 'mongoose';
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    minLength: [2, 'Name is too short!'],
-    maxLength: [55, 'Name is too long!'],
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minLength: [2, 'Name is too short!'],
+      maxLength: [55, 'Name is too long!'],
+      required: true,
+    },
+    password: { type: String, required: true },
+    email: { type: String, minLength: 5, maxLength: 50, required: true },
+    otp: { type: Number, minLength: 6, maxLength: 6 },
   },
-  password: { type: String, required: true },
-  email: { type: String, minLength: 5, maxLength: 50, required: true },
-  otp: { type: Number, minLength: 6, maxLength: 6 },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model('users', userSchema);
