9769ccc6c058cb011b757d3ffb8e2ac319a02db3
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-14 00:40:34 +0530

    refactor: user email validation

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,14 +1,24 @@
 import Ajv from 'ajv';
 import ajvErrors from 'ajv-errors';
+import apply from 'ajv-formats-draft2019';
 
 const ajv = new Ajv({ allErrors: true });
+apply(ajv, { formats: ['idn-email', 'iri'] });
 ajvErrors(ajv);
 
 const userValidations = {
   type: 'object',
   properties: {
     name: { type: 'string', minLength: 2, maxLength: 55 },
-    email: { type: 'string', minLength: 5, maxLength: 50 },
+    email: {
+      type: 'string',
+      format: 'idn-email',
+      minLength: 5,
+      maxLength: 50,
+      errorMessage: {
+        format: "Email format isn't valid, it must follow 'idn-email' format",
+      },
+    },
     password: {
       type: 'string',
       maxLength: 16,
@@ -26,10 +36,11 @@ const userValidations = {
 
 export default function UserValidation(req, res, next) {
   !ajv.validate(userValidations, req.body)
-    ? res.send({
+    ? res.status(400).send({
         error: ajv.errors,
         TimeStamp: Date(),
         handlerLocation: 'UserValidation',
+        message: "User data isn't valid",
       })
     : next();
 }
